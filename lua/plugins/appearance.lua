d00baddbf8104b5d0c78368d68adcd99f9e7af5a
Max Schopf <schopf.max@tutanota.com>
2024-07-15 11:18:29 +0200

    Inclusion of bubble theme for lualine

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -1,34 +1,86 @@
 return {
 
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			local colors = {
+				blue = "#80a0ff",
+				cyan = "#79dac8",
+				black = "#080808",
+				white = "#c6c6c6",
+				red = "#ff5189",
+				violet = "#d183e8",
+				grey = "#303030",
+			}
+			local bubbles_theme = {
+				normal = {
+					a = { fg = colors.black, bg = colors.violet },
+					b = { fg = colors.white, bg = colors.grey },
+					c = { fg = colors.white },
+				},
 
-  {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-      require "lualine".setup()
-    end
-  },
+				insert = { a = { fg = colors.black, bg = colors.blue } },
+				visual = { a = { fg = colors.black, bg = colors.cyan } },
+				replace = { a = { fg = colors.black, bg = colors.red } },
 
+				inactive = {
+					a = { fg = colors.white, bg = colors.black },
+					b = { fg = colors.white, bg = colors.black },
+					c = { fg = colors.white },
+				},
+			}
 
-  {
-    "willothy/nvim-cokeline",
-    dependencies = {
-      "nvim-lua/plenary.nvim",           -- Required for v0.4.0+
-      "nvim-tree/nvim-web-devicons",     -- If you want devicons
-      "stevearc/resession.nvim"          -- Optional, for persistent history
-    },
-    config = true
-  },
+			local opts = {
+				options = {
+					theme = bubbles_theme,
+					component_separators = "",
+					section_separators = { left = "", right = "" },
+				},
+				sections = {
+					lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+					lualine_b = { "filename", "branch" },
+					lualine_c = {
+						"%=", --[[ add your center compoentnts here in place of this comment ]]
+					},
+					lualine_x = {},
+					lualine_y = { "filetype", "progress" },
+					lualine_z = {
+						{ "location", separator = { right = "" }, left_padding = 3 },
+					},
+				},
+				inactive_sections = {
+					lualine_a = { "filename" },
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = { "location" },
+				},
+				tabline = {},
+				extensions = {},
+			}
 
-  {
-    "rachartier/tiny-devicons-auto-colors.nvim",
+			require("lualine").setup(opts)
+		end,
+	},
 
-    event = "VeryLazy",
-    config = function()
-      local theme_colors = require "catppuccin.palettes".get_palette("frappe")
-      require "tiny-devicons-auto-colors".setup(
-        { colors = theme_colors }
-      )
-    end
-  }
+	{
+		"willothy/nvim-cokeline",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- Required for v0.4.0+
+			"nvim-tree/nvim-web-devicons", -- If you want devicons
+			"stevearc/resession.nvim", -- Optional, for persistent history
+		},
+		config = true,
+	},
 
+	{
+		"rachartier/tiny-devicons-auto-colors.nvim",
+
+		event = "VeryLazy",
+		config = function()
+			local theme_colors = require("catppuccin.palettes").get_palette("frappe")
+			require("tiny-devicons-auto-colors").setup({ colors = theme_colors })
+		end,
+	},
 }
