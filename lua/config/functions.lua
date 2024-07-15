87069cf3e3a369f9c15e40d56ad202e35ae320f8
Max Schopf <schopf.max@tutanota.com>
2024-07-15 11:17:29 +0200

    Rename of function so that it actually works

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -2,5 +2,8 @@ local create = vim.api.nvim_create_user_command
 local opts = {}
 
 create("DapUiToggle", ':lua require"dapui".toggle()', opts)
-create("Check REPL for CMP-Dap", ":lua= require'dap'.sessions().capabilities.supportsCompletionsRequest", opts)
+create("ReplCheck", ":lua= require'dap'.sessions().capabilities.supportsCompletionsRequest", opts)
+
+
+
 
