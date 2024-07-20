c43d66b3763166cdd4db69b8290c6f5c084c72c5
Max Schopf <schopf.max@tutanota.com>
2024-07-20 21:40:30 +0200

    Formatted file

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -1,6 +1,5 @@
 local create = vim.api.nvim_create_user_command
 local opts = {}
-
 create("DapUiToggle", ':lua require"dapui".toggle()', opts)
 create("ReplCheck", ":lua= require'dap'.sessions().capabilities.supportsCompletionsRequest", opts)
 
