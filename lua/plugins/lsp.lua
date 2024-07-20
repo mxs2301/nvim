8408a827f65ca1c5656db6a1a53fd56cafe323db
Max Schopf <schopf.max@tutanota.com>
2024-07-20 21:42:26 +0200

    Formatted file after playing around
    I played around and tested some new changes and ended up not using them.
    This lead to the file getting heavily changed and having to be
    reformatted using the lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,98 +1,97 @@
 return {
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^4",
-    lazy = false,
-  },
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4",
+		lazy = false,
+	},
 
-  {
-    "Saecki/crates.nvim",
+	{
+		"Saecki/crates.nvim",
 
-    config = function()
-      local opts = {
-        completion = {
-          cmp = {
-            enabled = true,
-          },
-        },
-      }
-      require("crates").setup(opts)
-    end,
-  },
+		config = function()
+			local opts = {
+				completion = {
+					cmp = {
+						enabled = true,
+					},
+				},
+			}
+			require("crates").setup(opts)
+		end,
+	},
 
-  {
-    "p00f/clangd_extensions.nvim",
-  },
-  {
-    "ranjithshegde/ccls.nvim",
-  },
+	{
+		"p00f/clangd_extensions.nvim",
+	},
+	{
+		"ranjithshegde/ccls.nvim",
+	},
 
-  {
-    "mfussenegger/nvim-jdtls",
-  },
+	{
+		"mfussenegger/nvim-jdtls",
+	},
 
-  {
+	{
 
-    "neovim/nvim-lspconfig",
+		"neovim/nvim-lspconfig",
 
-    config = function()
-      -- Set icons
+		config = function()
+			-- Set icons
 
-      vim.diagnostic.config({
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = " ",
-            [vim.diagnostic.severity.WARN] = " ",
-            [vim.diagnostic.severity.HINT] = "󰠠 ",
-            [vim.diagnostic.severity.INFO] = " ",
-          },
-        },
-        virtual_text = {
-          prefix = "󰊠 ",
-        },
-      })
+			vim.diagnostic.config({
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = " ",
+						[vim.diagnostic.severity.WARN] = " ",
+						[vim.diagnostic.severity.HINT] = "󰠠 ",
+						[vim.diagnostic.severity.INFO] = " ",
+					},
+				},
+				virtual_text = {
+					prefix = "󰊠 ",
+				},
+			})
 
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lsp = require("lspconfig")
+			local lsp = require("lspconfig")
 
-      local server = {
-        --'clangd',
-        "lua_ls",
-        "texlab",
-        "typst_lsp",
-        "ccls",
-        --"scheme-langserver",
-      }
+			local server = {
+				--'clangd',
+				"lua_ls",
+				"texlab",
+				"typst_lsp",
+				"ccls",
+				--"scheme-langserver",
+			}
 
-      for _, value in pairs(server) do
-        lsp[value].setup({
-          capabilities = capabilities,
-        })
-      end
+			for _, value in pairs(server) do
+				lsp[value].setup({
+					capabilities = capabilities,
+				})
+			end
 
-      -- Place to setup server that needs more than standard settings
+			-- Place to setup server that needs more than standard settings
 
-      lsp["pylyzer"].setup({
-        capabilities = capabilities,
-        settings = {
-          python = {
-            checkOnType = true,
-            diagnostics = true,
-            inlayHints = true,
-            smartCompletion = true,
-          },
-        },
-      })
-      
+			lsp["pylyzer"].setup({
+				capabilities = capabilities,
+				settings = {
+					python = {
+						checkOnType = true,
+						diagnostics = true,
+						inlayHints = true,
+						smartCompletion = true,
+					},
+				},
+			})
 
-      -- lsp["clangd"].setup({
-      --   capabilities = capabilities,
-      --   on_attach = function()
-      --     require("clangd_extensions.inlay_hints").setup_autocmd()
-      --     require("clangd_extensions.inlay_hints").set_inlay_hints()
-      --   end,
-      -- })
-    end,
-  },
+			-- lsp["clangd"].setup({
+			--   capabilities = capabilities,
+			--   on_attach = function()
+			--     require("clangd_extensions.inlay_hints").setup_autocmd()
+			--     require("clangd_extensions.inlay_hints").set_inlay_hints()
+			--   end,
+			-- })
+		end,
+	},
 }
