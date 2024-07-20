1dd5100dcd84892a0e33573653854f1fd8498513
Max Schopf <schopf.max@tutanota.com>
2024-07-20 21:41:38 +0200

    Added plugins to have visual git changes in files

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,48 +1,57 @@
 return {
 
-	{
-		"SuperBo/fugit2.nvim",
-		lazy = true,
-		opts = {
-			width = 100,
-		},
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"nvim-lua/plenary.nvim",
-			{
-				"chrisgrieser/nvim-tinygit",
-				dependencies = {
-					"stevearc/dressing.nvim",
-				},
-			},
-		},
-		cmd = { "Fugit2", "Fugit2Diff", "Fugit2Graph", "Fugit2Blame" },
-	},
+  {
+    "SuperBo/fugit2.nvim",
+    lazy = true,
+    opts = {
+      width = 100,
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "nvim-lua/plenary.nvim",
+      {
+        "chrisgrieser/nvim-tinygit",
+        dependencies = {
+          "stevearc/dressing.nvim",
+        },
+      },
+    },
+    cmd = { "Fugit2", "Fugit2Diff", "Fugit2Graph", "Fugit2Blame" },
+  },
 
-	{
-		"kdheepak/lazygit.nvim",
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
-	{
-		"sindrets/diffview.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
 
-		cmd = {
-			"DiffviewFileHistory",
-			"DiffviewOpen",
-			"DiffviewToggleFiles",
-			"DiffviewFocusFiles",
-			"DiffviewRefresh",
-		},
-	},
+    cmd = {
+      "DiffviewFileHistory",
+      "DiffviewOpen",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+      "DiffviewRefresh",
+    },
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      local opts = {}
+      require("gitsigns").setup(opts)
+    end,
+  },
+  { "akinsho/git-conflict.nvim", version = "*", config = true },
 }
