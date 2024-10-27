dc0ad7ae732b155aaea4d716e4bab1d91b2321f6
Max Schopf <schopf.max@tutanota.com>
2024-10-27 10:58:40 +0100

    Adjust Latex Settings
    - Xelatex will now be used with Tectonic to support more modern features
    - Vimtex as a source got placed higher in the cmp-sources to better
      provide suggestions

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -2,8 +2,9 @@ return {
   {
     "lervag/vimtex",
     lazy = false,
-    init = function ()
-     vim.g.vimtex_view_method = 'zathura' 
+    init = function()
+      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_compiler_method = 'tectonic'
     end
   }
 }
