93387b0a6caebd32e0e962bf87db643760712350
Max Schopf <schopf.max@tutanota.com>
2024-10-27 10:54:27 +0100

    Add Neogit as new potential git client

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -21,6 +21,16 @@ return {
   },
 
   {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = true
+  },
+
+  {
     "kdheepak/lazygit.nvim",
     cmd = {
       "LazyGit",
