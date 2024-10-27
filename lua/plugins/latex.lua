b6a6bb138481e4ee44523a7cf967ca9a83adf8c0
Max Schopf <schopf.max@tutanota.com>
2024-10-27 23:05:14 +0100

    Add option to have two other backends
    Added the option to choose between arara and directives or
    self-contained tectonic

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -4,7 +4,10 @@ return {
     lazy = false,
     init = function()
       vim.g.vimtex_view_method = 'zathura'
-      vim.g.vimtex_compiler_method = 'tectonic'
+      -- Uncomment the engine to use
+      -- If none is uncommented latexmk with pdflatex will be used
+      -- vim.g.vimtex_compiler_method = 'arara'
+      -- vim.g.vimtex_compiler_method = 'tectonic' -- Just compiler
     end
   }
 }
