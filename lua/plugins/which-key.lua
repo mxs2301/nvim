230ed46652dc9b489c7f281aa885734f7e23782d
Max Schopf <schopf.max@tutanota.com>
2024-10-27 10:57:28 +0100

    Add Keybind to call Neogit

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -100,6 +100,7 @@ return {
           { "<leader>g",  group = "Git" },
           { "<leader>gc", ":Fugit2<cr>",  desc = "Open Git Client" },
           { "<leader>gl", ":LazyGit<cr>", desc = "Open LazyGit" },
+      {"<leader>gn", function () vim.cmd.Neogit() end, desc = "Open Neogit"},
         },
         {
           mode = "n",
