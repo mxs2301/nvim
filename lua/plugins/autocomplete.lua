5e517a980e7245a182c8b6467cbf93134c1ba3de
mxs2301 <90660823+mxs2301@users.noreply.github.com>
2024-10-28 14:25:58 +0100

    Fix completion bug
    Fixed the bug where cmdline ccompletion stopped working as the env source coudl not be found as a dependency

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -12,10 +12,6 @@ return {
   },
 
   {
-    "SergioRibera/cmp-dotenv",
-  },
-
-  {
 
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
@@ -23,6 +19,7 @@ return {
 
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
+      "SergioRibera/cmp-dotenv,
       --"hrsh7th/cmp-path",
       "FelipeLema/cmp-async-path",
       "hrsh7th/cmp-cmdline",
