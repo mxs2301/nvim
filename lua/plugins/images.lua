c79b2d782fbd54c605d40e0fcbfc96310c92755d
mxs2301 <90660823+mxs2301@users.noreply.github.com>
2024-10-28 08:29:48 +0100

    Create images.lua

---
+++ lua/plugins/images.lua
@@ -0,0 +1,13 @@
+local M  = {
+"HakonHarnes/img-clip.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add options here
+    -- or leave it empty to use the default settings
+  },
+  keys = {
+    -- suggested keymap
+  },
+}
+
+return M
