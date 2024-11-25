fbc47cfc8c401608a70ab961f1a61051e5fbde79
mxs2301 <90660823+mxs2301@users.noreply.github.com>
2024-11-25 13:51:04 +0100

    Update sessions.lua

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,21 +1,10 @@
 local M = {
 
   {
-    "gennaro-tedesco/nvim-possession",
-    dependencies = {
-      "ibhagwan/fzf-lua",
-    },
-    config = function()
-      local opts = {
-        autosave = true,
-        autoload = true,
-        autoswap = {
-          enable = true
-        },
-      }
-      require "nvim-possession".setup(opts)
-    end,
-  },
+    "olimorris/persisted.nvim",
+    lazy = false,
+    config = true,
+  }
 
 }
 
