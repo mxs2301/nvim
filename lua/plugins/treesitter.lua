7f1c5fe05125cea26d0d66a5b9641a1dfc756706
Max Schopf <max.schopf@mailfence.com>
2024-12-18 20:25:53 +0100

    Treesitter reformat

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,18 @@
 return {
 
-	{
-		"nvim-treesitter/nvim-treesitter"
-		, 
-		build = ":TSUpdate",
+  {
+    "nvim-treesitter/nvim-treesitter"
+    ,
+    build = ":TSUpdate",
 
-		config = function ()
-			local configs = require"nvim-treesitter.configs"
+    config = function()
+      local configs = require "nvim-treesitter.configs"
 
-			configs.setup({
-				ensure_installed = "all",
-				highlight = {enable = true},
-				indent = {enable = true}
-			})
-
-		end
-	}
+      configs.setup({
+        ensure_installed = "all",
+        highlight = { enable = true },
+        indent = { enable = true }
+      })
+    end
+  }
 }
